41435e9365297c6870ad7e81573b6d35c40c37f0
leechi <leechi2228@gmail.com>
2023-12-02 19:50:16 +0900

    feat(routing): Add default Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,55 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import Login from "./routes/Login";
+import CreateAccount from "./routes/Create-acount";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+])
+
 function App() {
 
   return (
     <>
-     
+    <GlobalStyles/>
+     <RouterProvider router={router}/>
     </>
   )
 }
